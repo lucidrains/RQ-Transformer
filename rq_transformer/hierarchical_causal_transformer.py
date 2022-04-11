84118b232d7a96dd5ee441d3eb3f1409f9ec4ae9
Phil Wang <lucidrains@gmail.com>
2022-04-11 09:10:13 -0700

    scaffold

---
+++ rq_transformer/hierarchical_causal_transformer.py
@@ -0,0 +1,264 @@
+import torch
+import torch.nn.functional as F
+from torch import nn, einsum
+from einops import rearrange, reduce, repeat
+
+# helpers
+
+def exists(val):
+    return val is not None
+
+def default(val, d):
+    return val if exists(val) else d
+
+def remainder_to_mult(num, mult):
+    return (mult - num % mult) % mult
+
+def log(t, eps = 1e-20):
+    return torch.log(t.clamp(min = eps))
+
+def gumbel_noise(t):
+    noise = torch.zeros_like(t).uniform_(0, 1)
+    return -log(-log(noise))
+
+def gumbel_sample(t, temperature = 1., dim = -1):
+    return ((t / temperature) + gumbel_noise(t)).argmax(dim = dim)
+
+def top_k(logits, thres = 0.5):
+    num_logits = logits.shape[-1]
+    k = max(int((1 - thres) * num_logits), 1)
+    val, ind = torch.topk(logits, k)
+    probs = torch.full_like(logits, float('-inf'))
+    probs.scatter_(1, ind, val)
+    return probs
+
+# helper classes
+
+def FeedForward(*, dim, mult = 4, dropout = 0.):
+    return nn.Sequential(
+        nn.LayerNorm(dim),
+        nn.Linear(dim, dim * mult),
+        nn.GELU(),
+        nn.Dropout(dropout),
+        nn.Linear(dim * mult, dim)
+    )
+
+class Attention(nn.Module):
+    def __init__(
+        self,
+        *,
+        dim,
+        dim_head = 64,
+        heads = 8,
+        dropout = 0.
+    ):
+        super().__init__()
+        self.scale = dim_head ** -0.5
+        self.heads = heads
+        inner_dim = dim_head * heads
+
+        self.dropout = nn.Dropout(dropout)
+        self.norm = nn.LayerNorm(dim)
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+        self.to_out = nn.Linear(inner_dim, dim, bias = False)
+
+    def forward(self, x):
+        h, device = self.heads, x.device
+
+        x = self.norm(x)
+        q, k, v = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), (q, k, v))
+
+        q = q * self.scale
+        sim = einsum('b h i d, b h j d -> b h i j', q, k)
+
+        i, j = sim.shape[-2:]
+        mask_value = -torch.finfo(sim.dtype).max
+        mask = torch.ones((i, j), dtype = torch.bool, device = device).triu(j - i + 1)
+        sim = sim.masked_fill(mask, mask_value)
+
+        sim = sim - sim.amax(dim = -1, keepdim = True).detach()
+        attn = sim.softmax(dim = -1)
+        attn = self.dropout(attn)
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
+class Transformer(nn.Module):
+    def __init__(
+        self,
+        *,
+        dim,
+        layers,
+        dim_head = 64,
+        heads = 8,
+        attn_dropout = 0.,
+        ff_dropout = 0.,
+        ff_mult = 4
+    ):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(layers):
+            self.layers.append(nn.ModuleList([
+                Attention(dim = dim, dim_head = dim_head, heads = heads, dropout = attn_dropout),
+                FeedForward(dim = dim, mult = ff_mult, dropout = ff_dropout)
+            ]))
+
+        self.norm = nn.LayerNorm(dim)
+
+    def forward(self, x):
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+
+        return self.norm(x)
+
+# main class
+
+class HierarchicalCausalTransformer(nn.Module):
+    def __init__(
+        self,
+        *,
+        num_tokens,
+        dim,
+        max_spatial_seq_len,
+        depth_seq_len,
+        spatial_layers,
+        depth_layers,
+        dim_head = 64,
+        heads = 8,
+        attn_dropout = 0.,
+        ff_mult = 4,
+        ff_dropout = 0.,
+        pad_id = 0
+    ):
+        super().__init__()
+        self.dim = dim
+        self.max_spatial_seq_len = max_spatial_seq_len
+        self.depth_seq_len = depth_seq_len
+
+        self.token_emb = nn.Embedding(num_tokens, dim)
+        self.spatial_start_token = nn.Parameter(torch.randn(dim))
+
+        self.spatial_pos_emb = nn.Embedding(max_spatial_seq_len, dim)
+        self.depth_pos_emb = nn.Embedding(depth_seq_len, dim)
+
+        self.spatial_transformer = Transformer(
+            dim = dim,
+            layers = spatial_layers,
+            dim_head = dim_head,
+            heads = heads,
+            attn_dropout = attn_dropout,
+            ff_dropout = ff_dropout,
+            ff_mult = ff_mult
+        )
+
+        self.depth_transformer = Transformer(
+            dim = dim,
+            layers = depth_layers,
+            dim_head = dim_head,
+            heads = heads,
+            attn_dropout = attn_dropout,
+            ff_dropout = ff_dropout,
+            ff_mult = ff_mult
+        )
+
+        self.to_logits = nn.Linear(dim, num_tokens)
+        self.pad_id = pad_id
+
+    def generate(self, prime = None, filter_thres = 0.9, temperature = 1., default_batch_size = 1):
+        total_seq_len = self.depth_seq_len * self.max_spatial_seq_len
+        device = next(self.parameters()).device
+
+        if not exists(prime):
+            prime = torch.empty((default_batch_size, 0), dtype = torch.long, device = device)
+
+        seq = prime
+
+        for _ in range(total_seq_len - seq.shape[-1]):
+            logits = self.forward(seq)[:, -1]
+            logits = top_k(logits, thres = filter_thres)
+            sampled = gumbel_sample(logits, dim = -1, temperature = temperature)
+            seq = torch.cat((seq, rearrange(sampled, 'b -> b 1')), dim = -1)
+
+        return rearrange(seq, 'b (s d) -> b s d', d = self.depth_seq_len)
+
+    def forward_empty(self, batch_size):
+        # take care of special case
+        # where you sample from input of 0 (start token only)
+
+        spatial_tokens = repeat(self.spatial_start_token, 'd -> b 1 d', b = batch_size)
+        depth_tokens = self.spatial_transformer(spatial_tokens)
+        depth_tokens = self.depth_transformer(depth_tokens)
+        return self.to_logits(depth_tokens)
+
+    def forward(self, ids, return_loss = False):
+        assert ids.ndim in {2, 3}
+        ids_orig_ndim = ids.ndim
+
+        if ids.numel() == 0:
+            return self.forward_empty(ids.shape[0])
+
+        if ids.ndim == 2:
+            # allow for ids to be given in the shape of (batch, seq)
+            # in which case it will be auto-padded to the next nearest multiple of depth seq len
+            seq_len = ids.shape[-1]
+            padding = remainder_to_mult(seq_len, self.depth_seq_len)
+            ids = F.pad(ids, (0, padding), value = self.pad_id)
+            ids = rearrange(ids, 'b (s d) -> b s d', d = self.depth_seq_len)
+
+        b, space, depth, device = *ids.shape, ids.device
+        assert space <= self.max_spatial_seq_len, 'spatial dimension is greater than the max_spatial_seq_len set'
+        assert depth == self.depth_seq_len, 'depth dimension must be equal to depth_seq_len'
+
+        tokens = self.token_emb(ids)
+
+        spatial_pos = self.spatial_pos_emb(torch.arange(space, device = device))
+        depth_pos = self.depth_pos_emb(torch.arange(depth, device = device))
+
+        tokens_with_depth_pos = tokens + depth_pos
+
+        # spatial tokens is tokens with depth pos reduced along depth dimension + spatial positions
+
+        spatial_tokens = reduce(tokens_with_depth_pos, 'b s d f -> b s f', 'sum') + spatial_pos
+
+        spatial_tokens = torch.cat((
+            repeat(self.spatial_start_token, 'f -> b 1 f', b = b),
+            spatial_tokens
+        ), dim = -2)
+
+        spatial_tokens = spatial_tokens[:, :-1]
+
+        spatial_tokens = self.spatial_transformer(spatial_tokens)
+
+        spatial_tokens = rearrange(spatial_tokens, 'b s f -> b s 1 f')
+
+        # spatial tokens become the start tokens of the depth dimension
+
+        depth_tokens = torch.cat((spatial_tokens, tokens_with_depth_pos), dim = -2)
+        depth_tokens = depth_tokens[:, :, :-1]
+
+        depth_tokens = rearrange(depth_tokens, '... n d -> (...) n d')
+
+        depth_tokens = self.depth_transformer(depth_tokens)
+
+        depth_tokens = rearrange(depth_tokens, '(b s) d f -> b s d f', b = b)
+        logits = self.to_logits(depth_tokens)
+
+        if ids_orig_ndim == 2:
+            logits = rearrange(logits, 'b ... n -> b (...) n')
+            logits = logits[:, :seq_len]
+
+        if not return_loss:
+            return logits
+
+        assert self.training
+
+        preds = rearrange(logits, 'b ... c -> b c (...)')
+
+        labels = rearrange(ids, 'b s d -> b (s d)')
+        labels = labels[:, :preds.shape[-1]]
+
+        loss = F.cross_entropy(preds, labels, ignore_index = self.pad_id)
+        return loss
