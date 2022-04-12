ee35ea5a5a8edccc29e745ffa392e60a1a590b85
Phil Wang <lucidrains@gmail.com>
2022-04-12 00:32:59 -0700

    bring in relative positional biases for hierarchical autoregressive transformer

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'RQ-transformer',
   packages = find_packages(exclude=[]),
-  version = '0.1.2',
+  version = '0.1.4',
   license='MIT',
   description = 'RQ Transformer - Autoregressive Transformer for Residual Quantized Codes',
   author = 'Phil Wang',
